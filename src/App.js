d2fa03442bc7eb3f5efae5b494c2875af222bd8a
Islam Maiche <islam.maiche@emploitic.com>
2022-03-02 01:21:18 +0100

    setup vidly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import {useCallback, useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [counter, setCounter] = useState(0);
+    const [counter2, setCounter2] = useState(0);
+
+
+    // const x = useCallback(() => {
+    //     setCounter(percounter => {
+    //         console.log(percounter);
+    //     })
+    // })
+
+    // useEffect(() => {
+    //   console.log('update 1 and 2 : ');
+    //   // x();
+    // }, [counter, counter2]);
+
+    // useEffect(() => {
+    //   console.log('update2 : ');
+    //   // x();
+    // }, [counter2]);
+
+    // useEffect(() => {
+    //     console.log(counter, 'kjhedkn');
+    //     // x()
+    // }, []);
+
+    // setCounter();
+
+    return (
+        <main className='container'>
+            <h1>Hello world</h1>
+            {/*<div>{counter}</div>*/}
+            {/*<button onClick={() => {*/}
+            {/*    setCounter(counter + 1)*/}
+            {/*}}>click here*/}
+            {/*</button>*/}
+            {/*<div>{counter2}</div>*/}
+            {/*<button onClick={() => {*/}
+            {/*    setCounter2(counter2 + 2)*/}
+            {/*}}>click here 2*/}
+            {/*</button>*/}
+        </main>
+    );
 }
 
 export default App;
