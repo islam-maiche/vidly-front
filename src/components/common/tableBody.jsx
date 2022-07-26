99acfd4e76c40523e025f5386c109223ba344257
Islam Maiche <islam.maiche@emploitic.com>
2022-07-26 16:24:26 +0100

    add table body component

---
+++ src/components/common/tableBody.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import _ from "lodash";
+
+const TableBody = (props) => {
+    const { data, columns } = props;
+
+    const renderCell = (item, column) => {
+        if (column.content) {
+            return column.content(item);
+        }
+
+        return _.get(item, column.path);
+    };
+
+    let createKey = (item, column) => {
+        return item._id + (column.path || column.key);
+    };
+
+    return (
+        <tbody>
+            {data.map((item) => (
+                <tr key={item._id}>
+                    {columns.map((column) => (
+                        <td key={createKey(item, column)}>
+                            {renderCell(item, column)}
+                        </td>
+                    ))}
+                </tr>
+            ))}
+        </tbody>
+    );
+};
+
+export default TableBody;
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Like from './common/like'
+import Like from "./common/like";
+import TableBody from "./common/tableBody";
 import TableHeader from "./common/tableHeader";
 
 const MoviesTable = (props) => {
@@ -10,41 +11,31 @@ const MoviesTable = (props) => {
         { path: "genre.name", label: "Genre" },
         { path: "numberInStock", label: "Stock" },
         { path: "dailyRentalRate", label: "Rate" },
-        { key: "like"},
-        { key: "Delete"},
+        {
+            key: "like",
+            content: movie => <Like liked={movie.liked} onClick={() => onLike(movie)} />,
+        },
+        {
+            key: "Delete",
+            content: movie => (
+                <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => onDelete(movie._id)}
+                >
+                    Delete
+                </button>
+            ),
+        },
     ];
 
     return (
         <table className="table table-responsive table-striped">
-            <TableHeader 
-                columns={columns} 
-                sortColumn={sortColumn} 
-                onSort={(sortColumn) => onSort(sortColumn)} 
+            <TableHeader
+                columns={columns}
+                sortColumn={sortColumn}
+                onSort={(sortColumn) => onSort(sortColumn)}
             />
-            <tbody>
-                {movies.map((movie) => (
-                    <tr key={movie._id}>
-                        <td>{movie.title}</td>
-                        <td>{movie.genre.name}</td>
-                        <td>{movie.numberInStock}</td>
-                        <td>{movie.dailyRentalRate}</td>
-                        <td>
-                            <Like
-                                liked={movie.liked}
-                                onClick={() => onLike(movie)}
-                            />
-                        </td>
-                        <td>
-                            <button
-                                className="btn btn-danger btn-sm"
-                                onClick={() => onDelete(movie._id)}
-                            >
-                                Delete
-                            </button>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
+            <TableBody data={movies} columns={columns} />
         </table>
     );
 };
